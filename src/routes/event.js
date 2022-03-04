41c53ecbe4ade8976e54c0642887e8a611363343
Dexter Marks-Barber <dexter@marks-barber.co.uk>
2022-03-04 12:05:17 +0000

    wip: Test returning word length

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,7 +1,27 @@
+import pusher from '$lib/services/pusher';
+import { random } from '$lib/utils/word';
+
 export async function post({ request }) {
+	const word = await random();
 	const { events } = await request.json();
 
-	console.log(events);
+	for (let i = 0; i < events.length; i++) {
+		const { channel, data, event, socket_id: socketId } = events[i];
+
+		let output = {};
+
+		switch (event) {
+			case 'client-new-word':
+				output.event = 'server-word-length';
+				output.body = word.length;
+
+				break;
+		}
+
+		console.log(output);
+
+		await pusher.trigger(channel, output.event, output.body, { socket_id: socketId });
+	}
 
 	return {
 		status: 204
